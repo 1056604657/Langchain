98782899744d074a8c740c1db58d7f76c9e726eb
huanghao <huanghao@bme.com>
2024-06-18 10:22:34 +0800

    提交代码

--- rag_knowledge/llama_index_test3.py
+++ rag_knowledge/llama_index_test3.py
@@ -29,11 +29,11 @@ import sys
 logging.basicConfig(stream=sys.stdout, level=logging.INFO)
 logging.getLogger().addHandler(logging.StreamHandler(stream=sys.stdout))
 
-Settings.llm = Ollama(model="wangshenzhi/llama3-8b-chinese-chat-ollama-q8:latest", base_url="http://10.10.10.15:11434",
-                      requst_timeout=600)
+Settings.llm = Ollama(model="qwen:110b", base_url="http://10.10.10.15:11434",
+                      request_timeout=600)
 #Settings.llm = Ollama(model="gpt-3.5-turbo", api_key="sk-Zekmsmp9EOEL9IfX432624CfCb0144AbB946875e9d15BeA8", api_base='https://api.xty.app/v1')
 #Settings.embed_model = OpenAIEmbedding(api_key="sk-Zekmsmp9EOEL9IfX432624CfCb0144AbB946875e9d15BeA8", api_base='https://api.xty.app/v1')
-Settings.embed_model = HuggingFaceEmbedding(model_name="BAAI/bge-small-en-v1.5")
+Settings.embed_model = HuggingFaceEmbedding(model_name="moka-ai/m3e-base")
 
 # Using the LlamaDebugHandler to print the trace of the sub questions
 # captured by the SUB_QUESTION callback event type
@@ -43,21 +43,35 @@ callback_manager = CallbackManager([llama_debug])
 Settings.callback_manager = callback_manager
 
 # load data
-pg_essay = SimpleDirectoryReader(input_dir="./data/xigua/").load_data()
+pg_essay = SimpleDirectoryReader(input_dir="./data/zj/").load_data()
+
+ay_essay = SimpleDirectoryReader(input_dir="./data/ay/").load_data()
 
 # build index and query engine
-vector_query_engine = VectorStoreIndex.from_documents(
+vector_query_engine1 = VectorStoreIndex.from_documents(
     pg_essay,
+    use_async=True,
+).as_query_engine()
+
+vector_query_engine2 = VectorStoreIndex.from_documents(
+    ay_essay,
     use_async=True,
 ).as_query_engine()
 
 # setup base query engine as tool
 query_engine_tools = [
     QueryEngineTool(
-        query_engine=vector_query_engine,
+        query_engine=vector_query_engine1,
         metadata=ToolMetadata(
-            name="西瓜",
-            description="西瓜与番茄的介绍"
+            name="长江钢铁",
+            description="介绍了安徽长江钢铁的评估情况",
+        )
+    ),
+    QueryEngineTool(
+        query_engine=vector_query_engine2,
+        metadata=ToolMetadata(
+            name="安阳钢铁",
+            description="介绍了安阳钢铁的评估情况",
         )
     )
 ]
@@ -68,7 +82,7 @@ query_engine = SubQuestionQueryEngine.from_defaults(
 )
 
 response = query_engine.query(
-    "番茄的形态特征的生长环境是什么?"
+    "安阳钢铁的无组织排放包含哪些方面?"
 )
 print(response)
 
